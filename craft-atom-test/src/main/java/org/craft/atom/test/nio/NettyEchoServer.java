b58c560a4a36194d72d39667f9d8f270e3dfb350
mindwind <mindwind@163.com>
2013-06-14 19:04:55 +0800

    new - add netty server logging config

--- craft-atom-test/src/main/java/org/craft/atom/test/nio/NettyEchoServer.java
+++ craft-atom-test/src/main/java/org/craft/atom/test/nio/NettyEchoServer.java
@@ -10,6 +10,10 @@ import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
 import org.jboss.netty.channel.socket.nio.NioWorkerPool;
 import org.jboss.netty.handler.execution.ExecutionHandler;
 import org.jboss.netty.handler.execution.OrderedMemoryAwareThreadPoolExecutor;
+import org.jboss.netty.handler.logging.LoggingHandler;
+import org.jboss.netty.logging.InternalLogLevel;
+import org.jboss.netty.logging.InternalLoggerFactory;
+import org.jboss.netty.logging.Log4JLoggerFactory;
 
 /**
  * @author Hu Feng
@@ -30,12 +34,14 @@ public class NettyEchoServer {
 			executorPool = Integer.toString(Runtime.getRuntime().availableProcessors());
 		}
 		
+		InternalLoggerFactory.setDefaultFactory(new Log4JLoggerFactory());
 		ChannelFactory factory = new NioServerSocketChannelFactory(Executors.newCachedThreadPool(), Integer.parseInt(ioPool), new NioWorkerPool(Executors.newCachedThreadPool(), Integer.parseInt(executorPool)));
 		ExecutionHandler executionHandler = new ExecutionHandler(new OrderedMemoryAwareThreadPoolExecutor(Integer.parseInt(executorPool), 0, 0));
 		ServerBootstrap bootstrap = new ServerBootstrap(factory);
 		NettyEchoHandler handler = new NettyEchoHandler();
 		ChannelPipeline pipeline = bootstrap.getPipeline();
 		pipeline.addFirst("executor", executionHandler);  
+		pipeline.addLast("logger", new LoggingHandler(InternalLogLevel.DEBUG));
 		pipeline.addLast("handler", handler);
 		bootstrap.setOption("child.receiveBufferSize", 2048);
 		bootstrap.bind(new InetSocketAddress(PORT));
