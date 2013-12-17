da378ef39e656aef1e7897bab3da83b9d1ea00a6
mindwind <mindwind@163.com>
2013-12-17 10:06:57 +0800

    fix - unify unit test convention for craft-atom-protocol-http

--- craft-atom-protocol-http/src/test/java/org/craft/atom/protocol/http/TestHttpParameterDecoder.java
+++ craft-atom-protocol-http/src/test/java/org/craft/atom/protocol/http/TestHttpParameterDecoder.java
@@ -5,26 +5,34 @@ import java.util.Map;
 
 import junit.framework.Assert;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.craft.atom.protocol.ProtocolException;
+import org.craft.atom.test.CaseCounter;
 import org.junit.Test;
 
 /**
  * @author mindwind
  * @version 1.0, Mar 25, 2013
  */
-public class HttpParameterDecoderTest {
+public class TestHttpParameterDecoder {
+	
+	
+	private static final Log LOG = LogFactory.getLog(TestHttpParameterDecoder.class);
+	
 	
 	private HttpParameterDecoder decoder = new HttpParameterDecoder();
 	private HttpParameterEncoder encoder = new HttpParameterEncoder();
 
+	
 	@Test
-	public void test() throws ProtocolException {
+	public void testParameter() throws ProtocolException {
 		String queryString = "a=aa&%E6%B5%8B%E8%AF%95=%E6%B5%8B%E8%AF%95value&a=bb";
 		List<Map<String, List<String>>> paralist = decoder.decode(queryString.getBytes());
 		Map<String, List<String>> paras = paralist.get(0);
 		Assert.assertEquals(2, paras.size());
-		
-		System.out.println(new String(encoder.encode(paras)));
+		LOG.debug(new String(encoder.encode(paras)));
+		System.out.println(String.format("[CRAFT-ATOM-PROTOCOL-HTTP] (^_^)  <%s>  Case -> test parameter. ", CaseCounter.incr(1)));
 	}
 	
 }
--- craft-atom-protocol-http/src/test/java/org/craft/atom/protocol/http/TestHttpRequestDecoder.java
+++ craft-atom-protocol-http/src/test/java/org/craft/atom/protocol/http/TestHttpRequestDecoder.java
@@ -6,9 +6,12 @@ import java.util.Random;
 
 import junit.framework.Assert;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.craft.atom.protocol.ProtocolException;
 import org.craft.atom.protocol.http.model.Cookie;
 import org.craft.atom.protocol.http.model.HttpRequest;
+import org.craft.atom.test.CaseCounter;
 import org.craft.atom.util.StringUtil;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,13 +20,19 @@ import org.junit.Test;
  * @author mindwind
  * @version 1.0, Feb 6, 2013
  */
-public class HttpRequestDecoderTest {
+public class TestHttpRequestDecoder {
 	
-	private HttpRequestEncoder encoder;
-	private HttpRequestDecoder decoder;
-	private Charset charset = Charset.forName("utf-8");
+	
+	private static final Log LOG = LogFactory.getLog(TestHttpRequestDecoder.class);
+	
+	
+	private HttpRequestEncoder encoder                            ;
+	private HttpRequestDecoder decoder                            ;
+	private Charset            charset  = Charset.forName("utf-8");
+	
 	
 	// ~ ------------------------------------------------------------------------------------------------------------
+	
 	
 	@Before
 	public void before() {
@@ -31,20 +40,17 @@ public class HttpRequestDecoderTest {
 		decoder = new HttpRequestDecoder();
 	}
 	
-	/**
-	 * Test for one request without entity
-	 */
-	@Test public void testOneRequestWithoutEntity() throws ProtocolException {
+	@Test 
+	public void testOneRequestWithoutEntity() throws ProtocolException {
 		String req = "\r\nGET /s?wd=java+jdk7&rsv_bp=0&inputT=14326 HTTP/1.1\r\nHost: www.baidu.com\r\nUser-Agent: Mozilla/5.0 (Windows NT 6.1; rv:5.0) Gecko/20100101 Firefox/5.0\r\nAccept: text/html,application/xhtml+xml,\r\n\tapplication/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: zh-cn,zh;q=0.5\r\nAccept-Encoding: gzip, deflate\r\nAccept-Charset: GB2312,utf-8;q=0.7,*;q=0.7\r\nConnection: keep-alive\r\nReferer: http://www.baidu.com/\r\nCookie: BAIDUID=34C25418C0B70D93E53A8E1CB8CB150F:FG=1\r\n\r\n";
 		List<HttpRequest> reqs = decoder.decode(req.getBytes(charset));
 		Assert.assertEquals(1, reqs.size());
-		System.out.println(new String(encoder.encode(reqs.get(0)), charset));
+		LOG.debug(new String(encoder.encode(reqs.get(0)), charset));
+		System.out.println(String.format("[CRAFT-ATOM-PROTOCOL-HTTP] (^_^)  <%s>  Case -> test one request without entity. ", CaseCounter.incr(1)));
 	}
 	
-	/**
-	 * Test for one and half request without entity
-	 */
-	@Test public void testOneAndHalfRequestWithoutEntity() throws ProtocolException {
+	@Test 
+	public void testOneAndHalfRequestWithoutEntity() throws ProtocolException {
 		String req = "\r\nGET /s?wd=java+jdk7&rsv_bp=0&inputT=14326 HTTP/1.1\r\nHost: www.baidu.com\r\nUser-Agent: Mozilla/5.0 (Windows NT 6.1; rv:5.0) Gecko/20100101 Firefox/5.0\r\nAccept: text/html,application/xhtml+xml,\r";
 		List<HttpRequest> reqs = decoder.decode(req.getBytes(charset));
 		Assert.assertEquals(0, reqs.size());
@@ -52,18 +58,17 @@ public class HttpRequestDecoderTest {
 		req = "\n\tapplication/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: zh-cn,zh;q=0.5\r\nAccept-Encoding: gzip, deflate\r\nAccept-Charset: GB2312,utf-8;q=0.7,*;q=0.7\r\nConnection: keep-alive\r\nReferer: http://www.baidu.com/\r\nCookie: BAIDUID=34C25418C0B70D93E53A8E1CB8CB150F:FG=1\r\n\r\nGET /s?wd=java+jdk7&rsv_bp=0&inputT=14326 HTTP/1.1\r";
 		reqs = decoder.decode(req.getBytes(charset));
 		Assert.assertEquals(1, reqs.size());
-		System.out.println(new String(encoder.encode(reqs.get(0)), charset));
+		LOG.debug(new String(encoder.encode(reqs.get(0)), charset));
 		
 		req = "\nAccept-Language: zh-cn,zh;q=0.5\r\n\r\n";
 		reqs = decoder.decode(req.getBytes(charset));
 		Assert.assertEquals(1, reqs.size());
-		System.out.println(new String(encoder.encode(reqs.get(0)), charset));
+		LOG.debug(new String(encoder.encode(reqs.get(0)), charset));
+		System.out.println(String.format("[CRAFT-ATOM-PROTOCOL-HTTP] (^_^)  <%s>  Case -> test one and half request without entity. ", CaseCounter.incr(3)));
 	}
 	
-	/**
-	 * Test for streaming request without entity
-	 */
-	@Test public void testStreamingRequestWithoutEntity() throws ProtocolException {
+	@Test 
+	public void testStreamingRequestWithoutEntity() throws ProtocolException {
 		String req = "GET";
 		List<HttpRequest> reqs = decoder.decode(req.getBytes(charset));
 		Assert.assertEquals(0, reqs.size());
@@ -87,74 +92,67 @@ public class HttpRequestDecoderTest {
 		req = "\nAccept: text/html,application/xhtml+xml,\r\n\tapplication/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: zh-cn,zh;q=0.5\r\nAccept-Encoding: gzip, deflate\r\nAccept-Charset: GB2312,utf-8;q=0.7,*;q=0.7\r\nConnection: keep-alive\r\nReferer: http://www.baidu.com/\r\nCookie: BAIDUID=34C25418C0B70D93E53A8E1CB8CB150F:FG=1\r\n\r\n";
 		reqs = decoder.decode(req.getBytes(charset));
 		Assert.assertEquals(1, reqs.size());
-		System.out.println(new String(encoder.encode(reqs.get(0)), charset));
+		LOG.debug(new String(encoder.encode(reqs.get(0)), charset));
 		
 		req = "GET /s?wd=java+jdk7&rsv_bp=0&inputT=14326 HTTP/1.1\r\nAccept-Language: zh-cn,zh;q=0.5\r\n\r\n";
 		reqs = decoder.decode(req.getBytes(charset));
 		Assert.assertEquals(1, reqs.size());
-		System.out.println(new String(encoder.encode(reqs.get(0)), charset));
+		LOG.debug(new String(encoder.encode(reqs.get(0)), charset));
+		System.out.println(String.format("[CRAFT-ATOM-PROTOCOL-HTTP] (^_^)  <%s>  Case -> test streaming request without entity. ", CaseCounter.incr(7)));
 	}
 	
-	/**
-	 * Test for streaming request without entity in a random loop
-	 */
-	@Test public void testStreamingRequestWithoutEntityInRandomLoop() throws ProtocolException {
+	
+	@Test 
+	public void testStreamingRequestWithoutEntityInRandomLoop() throws ProtocolException {
 		String req = "GET /s?wd=java+jdk7&rsv_bp=0&inputT=14326 HTTP/1.1\r\nHost: www.baidu.com\r\nUser-Agent: Mozilla/5.0 (Windows NT 6.1; rv:5.0) Gecko/20100101 Firefox/5.0\r\nAccept: text/html,application/xhtml+xml,\r\n\tapplication/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: zh-cn,zh;q=0.5\r\nAccept-Encoding: gzip, deflate\r\nAccept-Charset: GB2312,utf-8;q=0.7,*;q=0.7\r\nConnection: keep-alive\r\nReferer: http://www.baidu.com/\r\nCookie: BAIDUID=34C25418C0B70D93E53A8E1CB8CB150F:FG=1\r\n\r\n";	
 		testInRandomLoop(req, 100, false);
 		testInRandomLoop(req, 1, true);
+		System.out.println(String.format("[CRAFT-ATOM-PROTOCOL-HTTP] (^_^)  <%s>  Case -> test streaming request without entity in random loop. ", CaseCounter.incr(2)));
 	}
 	
-	/**
-	 * Test for one request with fix length entity
-	 */
-	@Test public void testOneRequestWithFixLengthEntity() throws ProtocolException {
+	@Test 
+	public void testOneRequestWithFixLengthEntity() throws ProtocolException {
 		String req = "\r\nPOST /craft/webservice/helloWS HTTP/1.1\r\nContent-Type: text/xml; charset=UTF-8\r\nAccept: */*\r\nSOAPAction: \"\"\r\nUser-Agent: Apache CXF 2.4.0\r\nCache-Control: no-cache\r\nPragma: no-cache\r\nHost: localhost:9999\r\nConnection: keep-alive\r\nContent-Length: 194\r\n\r\n<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\"><soap:Body><ns2:sayHi xmlns:ns2=\"http://craft.org\"><ns2:text>How are you?</ns2:text></ns2:sayHi></soap:Body></soap:Envelope>";
 		List<HttpRequest> reqs = decoder.decode(req.getBytes(charset));
 		
 		Assert.assertEquals(1, reqs.size());
-		System.out.println(new String(encoder.encode(reqs.get(0)), charset));
+		LOG.debug(new String(encoder.encode(reqs.get(0)), charset));
+		System.out.println(String.format("[CRAFT-ATOM-PROTOCOL-HTTP] (^_^)  <%s>  Case -> test one request with fix length entity. ", CaseCounter.incr(1)));
 	}
-	
-	/**
-	 * Test for streaming request with fix length entity in a random loop
-	 */
-	@Test public void testStreamingRequestWithFixLengthEntity() throws ProtocolException {
+
+	public void testStreamingRequestWithFixLengthEntity() throws ProtocolException {
 		String req = "\r\nPOST /craft/webservice/helloWS HTTP/1.1\r\nContent-Type: text/xml; charset=UTF-8\r\nAccept: */*\r\nSOAPAction: \"\"\r\nUser-Agent: Apache CXF 2.4.0\r\nCache-Control: no-cache\r\nPragma: no-cache\r\nHost: localhost:9999\r\nConnection: keep-alive\r\nContent-Length: 194\r\n\r\n<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\"><soap:Body><ns2:sayHi xmlns:ns2=\"http://craft.org\"><ns2:text>How are you?</ns2:text></ns2:sayHi></soap:Body></soap:Envelope>";
 		testInRandomLoop(req, 100, false);
 		testInRandomLoop(req, 1, true);
+		System.out.println(String.format("[CRAFT-ATOM-PROTOCOL-HTTP] (^_^)  <%s>  Case -> test streaming request with fix length entity. ", CaseCounter.incr(2)));
 	}
 	
-	/**
-	 * Test for streaming request with chunked entity in a random loop
-	 */
-	@Test public void testStreamingRequestWithChunkedEntity() throws ProtocolException {
+	@Test 
+	public void testStreamingRequestWithChunkedEntity() throws ProtocolException {
 		String req = "POST /craft/webservice/helloWS HTTP/1.1\r\nTransfer-Encoding: chunked\r\nTrailer: Content-MD5\r\nContent-Type: text/xml; charset=UTF-8\r\n\r\n24;extname=extvalue;aaa=bbb;kkk\r\nThis is the data in the first chunk \r\n1A\r\nand this is the second one\r\n0\r\nContent-MD5: gjqesdflj12dsfsf12\r\n";
 		testInRandomLoop(req, 100, false);
 		testInRandomLoop(req, 1, true);
+		System.out.println(String.format("[CRAFT-ATOM-PROTOCOL-HTTP] (^_^)  <%s>  Case -> test streaming request with chunked entity. ", CaseCounter.incr(2)));
 	}
 	
-	/**
-	 * Test for streaming request with chinese url and entity
-	 */
-	@Test public void testStreamingRequestWithChineseUrlAndEntity() throws ProtocolException {
+	@Test 
+	public void testStreamingRequestWithChineseUrlAndEntity() throws ProtocolException {
 		String req = "POST /测试/s?in=测试 HTTP/1.1\r\nHost: www.baidu.com\r\nContent-Length: 34\r\nUser-Agent: Mozilla/5.0 (Windows NT 6.1; rv:5.0) Gecko/20100101 Firefox/5.0\r\nAccept: text/html,application/xhtml+xml,\r\n\tapplication/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: zh-cn,zh;q=0.5\r\nAccept-Encoding: gzip, deflate\r\nAccept-Charset: GB2312,utf-8;q=0.7,*;q=0.7\r\nConnection: keep-alive\r\nReferer: http://www.baidu.com/\r\nCookie: BAIDUID=34C25418C0B70D93E53A8E1CB8CB150F:FG=1\r\n\r\ntest这是一个中文字符测试";
 		testInRandomLoop(req, 100, false);
 		testInRandomLoop(req, 1, true);
+		System.out.println(String.format("[CRAFT-ATOM-PROTOCOL-HTTP] (^_^)  <%s>  Case -> test streaming request with chinese url and entity. ", CaseCounter.incr(2)));
 	}
 	
-	/**
-	 * Test for folded header
-	 */
-	@Test public void testForFoldedHeader() throws ProtocolException {
+	@Test 
+	public void testForFoldedHeader() throws ProtocolException {
 		String req = "POST /craft/webservice/helloWS HTTP/1.1\r\nTransfer-Encoding: chunked\r\nTrailer: Content-MD5\r\nContent-Type: text/xml;\r\n \t charset=UTF-8\r\n\r\n24;extname=extvalue;aaa=bbb;kkk\r\nThis is the data in the first chunk \r\n1A\r\nand this is the second one\r\n0\r\nContent-MD5: gjqesdflj12dsfsf12\r\n";
 		testInRandomLoop(req, 100, false);
 		testInRandomLoop(req, 1, true);
+		System.out.println(String.format("[CRAFT-ATOM-PROTOCOL-HTTP] (^_^)  <%s>  Case -> test for folded header. ", CaseCounter.incr(2)));
 	}
 	
-	/**
-	 * Test for one request with cookie parse
-	 */
-	@Test public void testOneRequestWithCookie() throws ProtocolException {
+	@Test 
+	public void testOneRequestWithCookie() throws ProtocolException {
 		String req = "GET /s?wd=java+jdk7&rsv_bp=0&inputT=14326 HTTP/1.1\r\nHost: www.baidu.com\r\nUser-Agent: Mozilla/5.0 (Windows NT 6.1; rv:5.0) Gecko/20100101 Firefox/5.0\r\nAccept: text/html,application/xhtml+xml,\r\n\tapplication/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: zh-cn,zh;q=0.5\r\nAccept-Encoding: gzip, deflate\r\nAccept-Charset: GB2312,utf-8;q=0.7,*;q=0.7\r\nConnection: keep-alive\r\nReferer: http://www.baidu.com/\r\nCookie: SID=31d4d96e407aad42\r\nCookie: BAIDUID=34C25418C0B70D93E53A8E1CB8CB150F:FG=1; lang=en-US\r\n\r\n";
 		List<HttpRequest> reqs = decoder.decode(req.getBytes(charset));
 		Assert.assertEquals(1, reqs.size());
@@ -164,8 +162,9 @@ public class HttpRequestDecoderTest {
 		Assert.assertEquals(3, cookies.size());
 		
 		for (Cookie cookie : cookies) {
-			System.out.println(cookie.toHttpString());
+			LOG.debug(cookie.toHttpString());
 		}
+		System.out.println(String.format("[CRAFT-ATOM-PROTOCOL-HTTP] (^_^)  <%s>  Case -> test one request with cookie. ", CaseCounter.incr(2)));
 	}
 	
 	private void testInRandomLoop(String req, int loop, boolean onebyte) throws ProtocolException {
@@ -184,8 +183,8 @@ public class HttpRequestDecoderTest {
 			}
 			
 			Assert.assertEquals(1, reqs.size());
-			System.out.println("split num=" + num);
-			System.out.println(new String(encoder.encode(reqs.get(0)), charset));	
+			LOG.debug("split num=" + num);
+			LOG.debug(new String(encoder.encode(reqs.get(0)), charset));	
 		}
 	}
 
