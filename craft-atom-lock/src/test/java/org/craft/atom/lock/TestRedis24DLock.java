7791bea5ea640b1cc7dd03cc65ea70daa7b00b42
mindwind <mindwind@163.com>
2013-12-22 11:22:15 +0800

    new - unit test for craft-atom-lock

---
+++ craft-atom-lock/src/test/java/org/craft/atom/lock/TestRedis24DLock.java
@@ -0,0 +1,58 @@
+package org.craft.atom.lock;
+
+import java.util.concurrent.TimeUnit;
+
+import junit.framework.Assert;
+
+import org.craft.atom.test.CaseCounter;
+import org.junit.Before;
+import org.junit.Test;
+
+
+/**
+ * @author mindwind
+ * @version 1.0, Dec 22, 2013
+ */
+public class TestRedis24DLock extends AbstractDLockTests {
+	
+	
+	private String lockKey = "redis24.dlock";
+	
+	
+	// ~ -------------------------------------------------------------------------------------------------------------
+	
+	
+	public TestRedis24DLock() {
+		super();
+	}
+	
+	@Before
+	public void before() {
+		redis1.flushall();
+	}
+		
+	
+	// ~ -------------------------------------------------------------------------------------------------------------
+		
+	
+	@Test
+	public void testTryLockFalse() {
+		// WATCH: lock=true   GET: lock=true  -> LOCK: false
+		// WATCH: lock=true   GET: lock=false -> LOCK: false
+		// WATCH: lock=false  GET: lock=true  -> LOCK: false
+		redis1.set(lockKey, "1");
+		boolean b = dLock.tryLock(lockKey, 30, TimeUnit.SECONDS);
+		Assert.assertFalse(b);
+		System.out.println(String.format("[CRAFT-ATOM-REDIS] (^_^)  <%s>  Case -> test try lock false. ", CaseCounter.incr(1)));
+	}
+	
+	@Test
+	public void testTryLockTrue() {
+		// WATCH: lock=false  GET: lock=false -> LOCK: true
+		redis1.del(lockKey);
+		boolean b = dLock.tryLock(lockKey, 30, TimeUnit.SECONDS);
+		Assert.assertTrue(b);
+		System.out.println(String.format("[CRAFT-ATOM-REDIS] (^_^)  <%s>  Case -> test try lock true. ", CaseCounter.incr(1)));
+	}
+		
+}
