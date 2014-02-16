2cbc5933be4745318a572618359adf4a9c0687d5
mindwind <mindwind@163.com>
2014-02-16 21:00:56 +0800

    fix - rename default.js、header.html、footer.html

---
+++ js/craft-atom.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+	$('#header').load('/craft-atom-header.html');
+	$('#footer').load('/craft-atom-footer.html');
+});
--- js/default.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-	$('#header').load('/header.html');
-	$('#footer').load('/footer.html');
-});
