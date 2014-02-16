44927f4856d85206f18341db3f93373ac7fb61ae
mindwind <mindwind@163.com>
2014-02-16 21:52:16 +0800

    improve - new craft logo and css adjust

--- js/craft-atom.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-	$('#header').load('/craft-atom-header.html');
-	$('#footer').load('/craft-atom-footer.html');
-});
---
+++ js/craft.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+	$('#header').load('/craft-atom/craft-atom-header.html');
+	$('#footer').load('/craft-atom/craft-atom-footer.html');
+});
