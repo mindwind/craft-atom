988c05c77ca506d78ae975387a415889df91c805
mindwind <mindwind@163.com>
2014-04-09 21:38:43 +0800

    new - overload static factory method
    deprecate 2 methods

--- craft-atom-lock/src/main/java/org/craft/atom/lock/api/DLockFactory.java
+++ craft-atom-lock/src/main/java/org/craft/atom/lock/api/DLockFactory.java
@@ -27,13 +27,13 @@ public class DLockFactory {
 	 * @param redis
 	 * @return new distributed lock base on sharded redis version 2.4.x
 	 */
-	public static DLock newShardedRedis24DLock(ShardedRedisCommand redis) {
+	public static DLock newRedis24DLock(ShardedRedisCommand redis) {
 		return new ShardedRedis24DLock(redis);
 	}
 	
 	/**
 	 * @param redis
-	 * @return new distributed lock base on singleton redis version 2.6.x
+	 * @return new distributed lock base on singleton redis version 2.6.12+
 	 */
 	public static DLock newRedis26DLock(RedisCommand redis) {
 		return new Redis26DLock(redis);
@@ -42,6 +42,24 @@ public class DLockFactory {
 	/**
 	 * @param redis
 	 * @return new distributed lock base on sharded redis version 2.6.x
+	 */
+	public static DLock newRedis26DLock(ShardedRedisCommand redis) {
+		return new ShardedRedis26DLock(redis);
+	}
+	
+	/**
+	 * @deprecated
+	 * @param redis
+	 * @return new distributed lock base on sharded redis version 2.4.x
+	 */
+	public static DLock newShardedRedis24DLock(ShardedRedisCommand redis) {
+		return new ShardedRedis24DLock(redis);
+	}
+	
+	/**
+	 * @deprecated
+	 * @param redis
+	 * @return new distributed lock base on sharded redis version 2.6.12+
 	 */
 	public static DLock newShardedRedis26DLock(ShardedRedisCommand redis) {
 		return new ShardedRedis26DLock(redis);
