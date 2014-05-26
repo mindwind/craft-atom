13c19e84f2f2fb5b58a323fa6f51c31194719d38
mindwind <mindwind@163.com>
2014-05-26 18:21:53 +0800

    fix - log format of craft-atom-io

--- craft-atom-io/src/main/java/org/craft/atom/io/AbstractIoHandler.java
+++ craft-atom-io/src/main/java/org/craft/atom/io/AbstractIoHandler.java
@@ -20,37 +20,37 @@ abstract public class AbstractIoHandler implements IoHandler {
 	
 	@Override
 	public void channelOpened(Channel<byte[]> channel) {
-		LOG.debug("[CRAFT-ATOM-IO] Opened channel={}", channel);
+		LOG.debug("[CRAFT-ATOM-IO] Opened |channel={}|", channel);
 	}
 
 	@Override
 	public void channelClosed(Channel<byte[]> channel) {
-		LOG.debug("[CRAFT-ATOM-IO] Closed channel={}", channel); 
+		LOG.debug("[CRAFT-ATOM-IO] Closed |channel={}|", channel); 
 	}
 
 	@Override
 	public void channelIdle(Channel<byte[]> channel) {
-		LOG.debug("[CRAFT-ATOM-IO] Idle channel={}", channel);
+		LOG.debug("[CRAFT-ATOM-IO] Idle |channel={}|", channel);
 	}
 
 	@Override
 	public void channelRead(Channel<byte[]> channel, byte[] bytes) {
-		LOG.debug("[CRAFT-ATOM-IO] Read channel={}, bytes={}", channel, Arrays.toString(bytes));
+		LOG.debug("[CRAFT-ATOM-IO] Read |channel={}, bytes={}|", channel, Arrays.toString(bytes));
 	}
 
 	@Override
 	public void channelFlush(Channel<byte[]> channel, byte[] bytes) {
-		LOG.debug("[CRAFT-ATOM-IO] Flush channel={}, bytes={}", channel, Arrays.toString(bytes));
+		LOG.debug("[CRAFT-ATOM-IO] Flush |channel={}, bytes={}|", channel, Arrays.toString(bytes));
 	}
 
 	@Override
 	public void channelWritten(Channel<byte[]> channel, byte[] bytes) {
-		LOG.debug("[CRAFT-ATOM-IO] Written channel={}, bytes={}", channel, Arrays.toString(bytes));
+		LOG.debug("[CRAFT-ATOM-IO] Written |channel={}, bytes={}|", channel, Arrays.toString(bytes));
 	}
 
 	@Override
 	public void channelThrown(Channel<byte[]> channel, Throwable cause) {
-		LOG.warn("[CRAFT-ATOM-IO] Thrown channel={}", channel, cause);
+		LOG.warn("[CRAFT-ATOM-IO] Thrown |channel={}|", channel, cause);
 	}
 	
 }
