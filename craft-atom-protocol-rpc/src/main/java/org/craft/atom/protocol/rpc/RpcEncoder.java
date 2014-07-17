d21ed4975914ef834801b633e9ada91730f7c556
mindwind <mindwind@163.com>
2014-07-17 18:53:36 +0800

    new - craft-atom-protocol-rpc

---
+++ craft-atom-protocol-rpc/src/main/java/org/craft/atom/protocol/rpc/RpcEncoder.java
@@ -0,0 +1,12 @@
+package org.craft.atom.protocol.rpc;
+
+import org.craft.atom.protocol.AbstractProtocolEncoder;
+
+/**
+ * 
+ * @author mindwind
+ * @version 1.0, Jul 17, 2014
+ */
+public class RpcEncoder extends AbstractProtocolEncoder {
+
+}
---
+++ craft-atom-protocol-rpc/src/main/java/org/craft/atom/protocol/rpc/model/RpcMessage.java
@@ -0,0 +1,18 @@
+package org.craft.atom.protocol.rpc.model;
+
+import java.io.Serializable;
+
+/**
+ * RPC messages use generic message format for transferring data of RPC.
+ * <pre>
+ * 
+ * </pre>
+ * 
+ * @author mindwind
+ * @version 1.0, Jul 17, 2014
+ */
+public class RpcMessage implements Serializable {
+
+	private static final long serialVersionUID = 5138100956693144357L;
+
+}
