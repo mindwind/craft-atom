55f18a7da1b50dae015ade1cf4a31a3a2fa3f677
mindwind <mindwind@163.com>
2014-08-04 17:15:32 +0800

    new - rpc api definition

---
+++ craft-atom-rpc/src/main/java/org/craft/atom/rpc/api/RpcClient.java
@@ -0,0 +1,19 @@
+package org.craft.atom.rpc.api;
+
+/**
+ * RPC client
+ * 
+ * @author mindwind
+ * @version 1.0, Aug 4, 2014
+ */
+public interface RpcClient {
+
+	/**
+	 * Refer the rpc api proxy class.
+	 * 
+	 * @param clazz interface class
+	 * @return  a proxy.
+	 */
+	<T> T refer(Class<T> clazz);
+	
+}
---
+++ craft-atom-rpc/src/main/java/org/craft/atom/rpc/api/RpcServer.java
@@ -0,0 +1,17 @@
+package org.craft.atom.rpc.api;
+
+/**
+ * RPC server
+ * 
+ * @author mindwind
+ * @version 1.0, Jul 30, 2014
+ */
+public interface RpcServer {
+	
+	
+	/**
+	 * Starts the rpc server and gets things going.
+	 */
+	void serve();
+
+}
