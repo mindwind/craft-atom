39d79ec32beba880b3306479c67d31fa25307719
mindwind <mindwind@163.com>
2014-08-07 14:37:03 +0800

    improve - add fields

--- craft-atom-protocol-rpc/src/main/java/org/craft/atom/protocol/rpc/model/RpcBody.java
+++ craft-atom-protocol-rpc/src/main/java/org/craft/atom/protocol/rpc/model/RpcBody.java
@@ -10,9 +10,9 @@ import lombok.Setter;
 import lombok.ToString;
 
 /**
- * Represents a RPC header field.
+ * Represents a RPC body field.
  * <p>
- * A body that can be sent or received with a Rpc message, 
+ * A body that can be sent or received with a rpc message, 
  * but not all messages contain a body, it is optional.
  * The body contains a block of arbitrary data and can be serialized by specific serializer.
  * 
@@ -20,18 +20,21 @@ import lombok.ToString;
  * @version 1.0, Jul 18, 2014
  */
 @ToString
-@EqualsAndHashCode(of = { "clazz", "method", "argTypes", "args", "attachments" })
+@EqualsAndHashCode(of = { "clazz", "method", "argTypes", "args", "returnObject", "thrownObject", "attachments" })
 public class RpcBody implements Serializable {
 
 	
 	private static final long serialVersionUID = 5138100956693144357L;
 	
 	
-	@Getter @Setter private Class<?>            clazz      ;
-	@Getter @Setter private String              method     ;
-	@Getter         private Class<?>[]          argTypes   ;
-	@Getter         private Object[]            args       ;
-	@Getter @Setter private Map<String, Object> attachments;
+	@Getter @Setter private Class<?>            clazz       ;
+	@Getter @Setter private String              method      ;
+	@Getter         private Class<?>[]          argTypes    ;
+	@Getter         private Object[]            args        ;
+	@Getter @Setter private Object              returnObject;
+	@Getter @Setter private Throwable           thrownObject;
+	@Getter @Setter private Map<String, Object> attachments ;
+	private int test;
 	
 	
 	public void addAttachment(String key, String value) {
