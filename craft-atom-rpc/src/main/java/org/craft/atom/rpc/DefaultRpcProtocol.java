6fa95585b74e36b22013d27b2f108d7752872a35
mindwind <mindwind@163.com>
2014-08-08 16:51:50 +0800

    improve - implement RpcProtocol

--- craft-atom-rpc/src/main/java/org/craft/atom/rpc/DefaultRpcProtocol.java
+++ craft-atom-rpc/src/main/java/org/craft/atom/rpc/DefaultRpcProtocol.java
@@ -2,6 +2,7 @@ package org.craft.atom.rpc;
 
 import org.craft.atom.protocol.ProtocolDecoder;
 import org.craft.atom.protocol.ProtocolEncoder;
+import org.craft.atom.protocol.rpc.api.RpcCodecFactory;
 import org.craft.atom.protocol.rpc.model.RpcMessage;
 import org.craft.atom.rpc.spi.RpcProtocol;
 
@@ -10,17 +11,24 @@ import org.craft.atom.rpc.spi.RpcProtocol;
  * @version 1.0, Aug 7, 2014
  */
 public class DefaultRpcProtocol implements RpcProtocol {
+	
+	
+	private ProtocolEncoder<RpcMessage> encoder;
+	
+	
+	public DefaultRpcProtocol() {
+		this.encoder = RpcCodecFactory.newRpcEncoder();
+	}
+	
 
 	@Override
 	public ProtocolEncoder<RpcMessage> getRpcEncoder() {
-		// TODO Auto-generated method stub
-		return null;
+		return encoder;
 	}
 
 	@Override
 	public ProtocolDecoder<RpcMessage> getRpcDecoder() {
-		// TODO Auto-generated method stub
-		return null;
+		return RpcCodecFactory.newRpcDecoder();
 	}
 
 }
