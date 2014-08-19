8da74b462472f73d9a40cf78862d550d897f113d
mindwind <mindwind@163.com>
2014-08-19 11:44:24 +0800

    new - RpcFuture interface

---
+++ craft-atom-rpc/src/main/java/org/craft/atom/rpc/spi/RpcFuture.java
@@ -0,0 +1,39 @@
+package org.craft.atom.rpc.spi;
+
+import java.util.concurrent.TimeUnit;
+
+import org.craft.atom.protocol.rpc.model.RpcMessage;
+
+/**
+ * Represents the completion of an asynchronous rpc invocation.
+ * 
+ * @author mindwind
+ * @version 1.0, Aug 19, 2014
+ */
+public interface RpcFuture {
+	
+	
+	/**
+	 * Wait for the asynchronous operation to complete with the specified timeout.
+	 * 
+	 * @param timeout
+	 * @param unit
+	 * @return <tt>true</tt> if the operation is completed.
+	 * @throws InterruptedException
+	 */
+	boolean await(long timeout, TimeUnit unit) throws InterruptedException;
+	
+	/**
+     * Returns the cause of the rpc failure if and only if the rpc operation has failed due to an {@link Exception}.  
+     * Otherwise, <tt>null</tt> is returned.
+     */
+    Throwable getThrowable();
+    
+    /**
+     * Returns the rpc response message, it returns <tt>null</tt> if this future is not ready.
+     * @return rpc response message.
+     */
+    RpcMessage getRpcMessage();
+
+    
+}
