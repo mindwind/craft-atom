b6a4587f405d37ef635b3c0c73dfe0d994718001
mindwind <mindwind@163.com>
2014-09-15 17:29:10 +0800

    fix - java doc redundant blank line

--- craft-atom-rpc/src/main/java/org/craft/atom/rpc/api/RpcServer.java
+++ craft-atom-rpc/src/main/java/org/craft/atom/rpc/api/RpcServer.java
@@ -7,8 +7,7 @@ import org.craft.atom.protocol.rpc.model.RpcMethod;
  * <p>
  * Use {@link RpcFactory} creates a rpc server and expose the remote interfaces or methods.
  * Last invoke {@link RpcServer#serve()} to start the server to serve the rpc request from {@link RpcClient}.
- * 
- * 
+ *  
  * @author mindwind
  * @version 1.0, Jul 30, 2014
  */
