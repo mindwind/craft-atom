1bf4d199e3b0a3b6d8eab3ac14f5ba090724f2d7
mindwind <mindwind@163.com>
2014-10-11 11:42:25 +0800

    new - spi definition

---
+++ craft-atom-rpc/src/main/java/org/craft/atom/rpc/spi/RpcEntry.java
@@ -0,0 +1,28 @@
+package org.craft.atom.rpc.spi;
+
+import org.craft.atom.rpc.api.RpcParameter;
+
+/**
+ * RPC entry, used by {@link RpcRegistry} to encapsulate exported remote API.
+ * 
+ * @author mindwind
+ * @version 1.0, Oct 11, 2014
+ */
+public interface RpcEntry {
+
+	/**
+	 * @return rpc entry key
+	 */
+	String getKey();
+	
+	/**
+	 * @return implementor object of rpc interface
+	 */
+	Object getRpcObject();
+	
+	/**
+	 * @return rpc behavioral parameter
+	 */
+	RpcParameter getRpcParameter();
+	
+}
---
+++ craft-atom-rpc/src/main/java/org/craft/atom/rpc/spi/RpcRegistry.java
@@ -0,0 +1,34 @@
+package org.craft.atom.rpc.spi;
+
+
+
+/**
+ * RPC registry
+ * 
+ * @author mindwind
+ * @version 1.0, Oct 11, 2014
+ */
+public interface RpcRegistry {
+
+	/**
+	 * Register a entry.
+	 * 
+	 * @param entry
+	 */
+	void register(RpcEntry entry);
+	
+	/**
+	 * Unregister a entry.
+	 * 
+	 * @param entry
+	 */
+	void unregister(RpcEntry entry);
+	
+	/**
+	 * Lookup a rpc entry by the entry key.
+	 * 
+	 * @param key the entry key
+	 * @return
+	 */
+	RpcEntry lookup(String key);
+}
