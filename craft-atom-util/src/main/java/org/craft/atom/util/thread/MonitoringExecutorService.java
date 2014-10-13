5eb4432775a9cf8c1b9387cc25e71b193aca5487
mindwind <mindwind@163.com>
2014-10-13 18:09:46 +0800

    new - MonitoringExecutorService

---
+++ craft-atom-util/src/main/java/org/craft/atom/util/thread/MonitoringExecutorService.java
@@ -0,0 +1,28 @@
+package org.craft.atom.util.thread;
+
+import java.util.concurrent.ExecutorService;
+
+/**
+ * An {@link ExecutorService} that provides extra monitoring function.
+ * 
+ * @author mindwind
+ * @version 1.0, Oct 13, 2014
+ */
+public interface MonitoringExecutorService extends ExecutorService {
+	
+	/**
+	 * @return the approximate wait task count of the executor.
+	 */
+	int waitCount();
+	
+	/**
+	 * @return the approximate executing count of the executor.
+	 */
+	int executingCount();
+	
+	/**
+	 * @return the approximate complete count of the executor.
+	 */
+	long completeCount();
+	
+}
---
+++ craft-atom-util/src/main/java/org/craft/atom/util/thread/MonitoringThreadPoolExecutor.java
@@ -0,0 +1,51 @@
+package org.craft.atom.util.thread;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author mindwind
+ * @version 1.0, Oct 13, 2014
+ */
+public class MonitoringThreadPoolExecutor extends ThreadPoolExecutor implements MonitoringExecutorService {
+
+	
+	public MonitoringThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, RejectedExecutionHandler handler) {
+		super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, handler);
+	}
+
+	public MonitoringThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, ThreadFactory threadFactory, RejectedExecutionHandler handler) {
+		super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory, handler);
+	}
+
+	public MonitoringThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, ThreadFactory threadFactory) {
+		super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory);
+	}
+
+	public MonitoringThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue) {
+		super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
+	}
+	
+	
+	// ~ -------------------------------------------------------------------------------------------------------------
+
+	
+	@Override
+	public int waitCount() {
+		return getQueue().size();
+	}
+
+	@Override
+	public int executingCount() {
+		return getActiveCount();
+	}
+
+	@Override
+	public long completeCount() {
+		return getCompletedTaskCount();
+	}
+
+}
